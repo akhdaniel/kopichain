5f6174fb43fd95c8057d71052f9edadf2253143f
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2023-07-29 11:41:33 +0700

    front

---
+++ front/src/components/ContractInteraction.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <h2>Interaksi dengan Smart Contract</h2>
+    <p>Saldo Tabungan Anda: {{ savingsBalance }}</p>
+    <button @click="depositSavings">Tambah Tabungan</button>
+    <button @click="withdrawSavings">Ambil Tabungan</button>
+    <button @click="getBalance">Lihat Saldo</button>
+  </div>
+</template>
+
+<script>
+import web3 from "../web3Config";
+
+export default {
+  data() {
+    return {
+      savingsBalance: 0,
+    };
+  },
+  methods: {
+    async depositSavings() {
+      const contractAddress = "0x..."; // Ganti dengan alamat smart contract Anda
+      const contractABI = []; // Ganti dengan ABI smart contract Anda
+
+      const contract = new web3.eth.Contract(contractABI, contractAddress);
+      // Panggil fungsi depositSavings di smart contract
+      // Misalnya: await contract.methods.depositSavings(100).send({ from: yourAddress });
+    },
+    async withdrawSavings() {
+      // Implementasi pengambilan tabungan dari smart contract
+    },
+    async getBalance() {
+      // Implementasi untuk mendapatkan saldo tabungan dari smart contract
+    },
+  },
+};
+</script>
---
+++ front/src/web3config.js
@@ -0,0 +1,6 @@
+import Web3 from "web3";
+
+const rpcUrl = "https://bsc-dataseed.binance.org/"; // Ganti dengan URL jaringan Binance atau Arbitrum
+const web3 = new Web3(rpcUrl);
+
+export default web3;
