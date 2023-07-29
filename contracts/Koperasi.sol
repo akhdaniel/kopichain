a8a3928ecacf468d68691a1a5a91848591e8e5fb
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2023-07-29 11:18:15 +0700

    koperasi.sol

--- contracts/Koperasi.sol
+++ contracts/Koperasi.sol
@@ -3,8 +3,7 @@ pragma solidity ^0.8.0;
 
 contract Koperasi {
     address public owner;
-    
-    // Struktur data untuk informasi anggota
+
     struct Member {
         string name;
         string email;
@@ -13,34 +12,18 @@ contract Koperasi {
         uint256 dateOfBirth;
         uint8 gender;
         bool isActive;
+        uint256 savingsBalance; // Saldo tabungan anggota
+        uint256 lastDepositTime; // Waktu terakhir melakukan deposit
     }
 
-    // Struktur data untuk pinjaman
-    struct Loan {
-        address borrower;
-        uint256 amount;
-        uint256 interestRate;
-        uint256 duration;
-        bool isApproved;
-        bool isPaid;
-    }
-
-    // Mapping untuk menyimpan data anggota berdasarkan alamat Ethereum mereka
     mapping(address => Member) public members;
-    
-    // Array untuk menyimpan alamat anggota secara terurut
     address[] public memberAddresses;
-    
-    // Array untuk menyimpan data pinjaman
-    Loan[] public loans;
 
-    // Modifier untuk membatasi akses hanya untuk pemilik kontrak
     modifier onlyOwner() {
         require(msg.sender == owner, "Hanya pemilik kontrak yang dapat melakukan ini");
         _;
     }
 
-    // Constructor kontrak
     constructor() {
         owner = msg.sender;
     }
@@ -55,7 +38,7 @@ contract Koperasi {
         uint8 _gender
     ) public {
         require(members[msg.sender].dateOfBirth == 0, "Anda sudah terdaftar sebagai anggota");
-        
+
         Member memory newMember = Member({
             name: _name,
             email: _email,
@@ -63,48 +46,50 @@ contract Koperasi {
             addressInfo: _addressInfo,
             dateOfBirth: _dateOfBirth,
             gender: _gender,
-            isActive: true
+            isActive: true,
+            savingsBalance: 0,
+            lastDepositTime: block.timestamp
         });
 
         members[msg.sender] = newMember;
         memberAddresses.push(msg.sender);
     }
 
-    // Fungsi untuk mengajukan pinjaman
-    function applyLoan(
-        uint256 _amount,
-        uint256 _interestRate,
-        uint256 _duration
-    ) public {
-        require(members[msg.sender].isActive, "Anda harus menjadi anggota aktif untuk mengajukan pinjaman");
-
-        Loan memory newLoan = Loan({
-            borrower: msg.sender,
-            amount: _amount,
-            interestRate: _interestRate,
-            duration: _duration,
-            isApproved: false,
-            isPaid: false
-        });
-
-        loans.push(newLoan);
+    // Fungsi untuk menambah tabungan
+    function depositSavings(uint256 _amount) public {
+        require(members[msg.sender].isActive, "Anda harus menjadi anggota aktif untuk menambah tabungan");
+        members[msg.sender].savingsBalance += _amount;
+        members[msg.sender].lastDepositTime = block.timestamp;
     }
 
-    // Fungsi untuk menyetujui pinjaman (hanya pemilik kontrak)
-    function approveLoan(uint256 _loanIndex) public onlyOwner {
-        require(_loanIndex < loans.length, "Indeks pinjaman tidak valid");
-        loans[_loanIndex].isApproved = true;
+    // Fungsi untuk mengambil tabungan
+    function withdrawSavings(uint256 _amount) public {
+        require(members[msg.sender].isActive, "Anda harus menjadi anggota aktif untuk mengambil tabungan");
+        require(members[msg.sender].savingsBalance >= _amount, "Saldo tabungan tidak cukup");
+        members[msg.sender].savingsBalance -= _amount;
     }
 
-    // Fungsi untuk menandai pinjaman sebagai telah dibayar
-    function markLoanAsPaid(uint256 _loanIndex) public onlyOwner {
-        require(_loanIndex < loans.length, "Indeks pinjaman tidak valid");
-        loans[_loanIndex].isPaid = true;
+    // Fungsi untuk melihat saldo tabungan
+    function getBalance() public view returns (uint256) {
+        return members[msg.sender].savingsBalance;
     }
 
-    // Fungsi untuk mengubah status anggota (aktif/nonaktif)
-    function changeMemberStatus(address _memberAddress, bool _isActive) public onlyOwner {
-        members[_memberAddress].isActive = _isActive;
+    // Fungsi untuk menghitung bunga tabungan per bulan (misal: 2% per bulan)
+    function calculateMonthlyInterest(uint256 _amount) public pure returns (uint256) {
+        uint256 interestRate = 2; // Bunga tabungan per bulan (misal: 2%)
+        uint256 monthlyInterest = (_amount * interestRate) / 100;
+        return monthlyInterest;
+    }
+
+    // Fungsi untuk menghitung bunga dan menambahkan ke saldo tabungan
+    function addInterestToSavings() public {
+        require(members[msg.sender].isActive, "Anda harus menjadi anggota aktif untuk menambahkan bunga tabungan");
+        uint256 currentTime = block.timestamp;
+        uint256 lastDepositTime = members[msg.sender].lastDepositTime;
+        uint256 timeDiff = currentTime - lastDepositTime;
+        uint256 monthsPassed = timeDiff / 30 days; // Mengasumsikan 30 hari per bulan
+        uint256 interest = calculateMonthlyInterest(members[msg.sender].savingsBalance) * monthsPassed;
+        members[msg.sender].savingsBalance += interest;
+        members[msg.sender].lastDepositTime = currentTime;
     }
 }
-
