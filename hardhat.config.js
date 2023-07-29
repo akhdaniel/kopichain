3bf7dba8e395ee5e85fbad46748ae30e07ff587b
akhmad daniel sembiring <akhmad.daniel@gmail.com>
2023-07-29 11:06:25 +0700

    hardhat

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
+require("@nomicfoundation/hardhat-toolbox");
+
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.19",
 };
